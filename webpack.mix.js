7223db34062108c49f26bcd257d54a617aaefc49
raulTahicheDaw <raul.tahiche.daw@gmail.com>
2018-05-08 01:51:01 +0100

    Segundo Commit. Modal Clientes

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,16 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/assets/plantillas/css/font-awesome.min.css',
+    'resources/assets/plantillas/css/simple-line-icons.min.css',
+    'resources/assets/plantillas/css/style.css'
+], 'public/css/all.css')
+.scripts([
+    'resources/assets/plantillas/js/jquery.min.js',
+    'resources/assets/plantillas/js/popper.min.js',
+    'resources/assets/plantillas/js/bootstrap.min.js',
+    'resources/assets/plantillas/js/pace.min.js',
+    'resources/assets/plantillas/js/Chart.min.js',
+    'resources/assets/plantillas/js/template.js',
+], 'public/js/all.js');
